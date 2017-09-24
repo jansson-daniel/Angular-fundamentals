9b95884fd097ebaa1915466095094b878f555fda
Daniel <janzon.daniel@gmail.com>
2017-09-24 16:53:23 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <div>
+      <app-simple-form></app-simple-form>
+    </div>`
 })
 export class AppComponent {
   title = 'app works!';
