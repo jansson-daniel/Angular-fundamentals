4ee9c30460b721c68f35a578bb506de2cb8cc72f
dhanisthlm <dhanisthlm@gmail.com>
2017-09-24 21:33:23 +0200

    Adding css to component, Lesson 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,17 @@ import { Inject } from '@angular/core';
       <ul>
         <li *ngFor="let message of mail.messages">{{ message.text }}</li>
       </ul>
-			<ul>
-        <app-simple-form
-          *ngFor="let message of mail.messages"
-          [message]="message.text"
-          (update)="onUpdate(message.id, $event.text)"
-        >
-        </app-simple-form>
-      </ul>
-		</div>`
+      <app-simple-form
+        *ngFor="let message of mail.messages"
+        [message]="message.text"
+        (update)="onUpdate(message.id, $event.text)">
+      </app-simple-form>
+		</div>`,
+  styles: [`
+    app-simple-form {
+      margin-bottom: 10px
+    }
+  `]
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -7,7 +7,16 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
       <input #myInput type="text" [(ngModel)]="message" />
       <button (click)="update.emit({ text: message })">Click me!</button>
     </div>`,
-  styles: []
+  styles: [`
+    :host {
+      display: flex;
+      flex-direction: column;
+    }
+    
+    * {
+      font-family: 'Arial', 'sans-serif';
+    }
+  `]
 })
 export class SimpleFormComponent implements OnInit {
 
